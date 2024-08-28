8b11b167d336f8688ba0b0d231133fac62701ff6
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-28 16:06:29 +0200

    Add try catch block to handle validation errors

--- models/task.js
+++ models/task.js
@@ -5,7 +5,8 @@ const TaskSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Must provide name'],
     trim: true,
-    maxlength: [20, 'Name cannot be more than 20 characters'],
+    maxlength: [90, 'Name cannot be more than 90 characters'],
+    minlength: [5, 'Name cannot be less than 5 characters'],
   },
   completed: {
     type: Boolean,
