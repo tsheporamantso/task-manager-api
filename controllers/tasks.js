340e0d5a0a93e76426541899477bf25110dc9643
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-29 17:13:04 +0200

    Add update functionality

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -25,19 +25,53 @@ const getTask = async (req, res) => {
     if (task) {
       res.status(200).json({ task });
     } else {
-      res.status(404).json({ msg: `No task with id: ${taskID}` });
+      res
+        .status(404)
+        .json({ success: false, msg: `No task with id: ${taskID}` });
     }
   } catch (error) {
     res.status(500).json({ msg: error });
   }
 };
 
-const updateTask = (req, res) => {
-  res.send('Update a task');
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (task) {
+      res.status(200).json({ task });
+    } else {
+      res
+        .status(404)
+        .json({
+          success: false,
+          msg: `Task id ${taskID} could not be updated!`,
+        });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send('Delete task');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (task) {
+      // res.status(200).json({ task });
+      // res.status(200).send();
+      res.status(200).json({ task: null, status: 'success' });
+    } else {
+      res
+        .status(404)
+        .json({ success: false, msg: `No task with id: ${taskID}` });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
