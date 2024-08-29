1bcd372813ce2d2ebee58128fd72313e08499b28
tsheporamantso <tshepo.ramantso@outlook.com>
2024-08-29 20:50:11 +0200

    Add custome error handling function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/task');
 const asyncWrapper = require('../middleware/async');
+const { createCustomError } = require('../errors/custom-error');
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -11,17 +12,17 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (task) {
     res.status(200).json({ task });
   } else {
-    res.status(404).json({ msg: `No task with id: ${taskID}` });
+    next(createCustomError(`No task with id: ${taskID}`, 404));
   }
 });
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
     new: true,
@@ -30,19 +31,17 @@ const updateTask = asyncWrapper(async (req, res) => {
   if (task) {
     res.status(200).json({ task });
   } else {
-    res.status(404).json({
-      msg: `No task with id: ${taskID}`,
-    });
+    next(createCustomError(`No task with id: ${taskID}`, 404));
   }
 });
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (task) {
     res.status(200).json({ task });
   } else {
-    res.status(404).json({ msg: `No task with id: ${taskID}` });
+    next(createCustomError(`No task with id: ${taskID}`, 404));
   }
 });
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,10 @@
+const { CustomAPIError } = require('../errors/custom-error');
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  res.status(500).json({ msg: err });
+  if (err instanceof CustomAPIError) {
+    res.status(err.statusCode).json({ msg: err.message });
+  }
+  res.status(500).json({ msg: 'Something went wrong please try again!' });
   next();
 };
 
